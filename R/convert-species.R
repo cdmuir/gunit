9e0a6cd603edb87051e20a3df4a7310abc695bf6
cdmuir <chrisdmuir@gmail.com>
2022-10-14 07:06:40 -1000

    fixed errors, warnings, notes

--- R/convert-species.R
+++ R/convert-species.R
@@ -2,11 +2,12 @@
 #'
 #' @param g_w conductance to water vapor in units
 #' (\eqn{\mu}mol H2O / (m^2 s Pa)) of class \code{units}.
-#' @param D_c diffusion coefficient for CO2 in air in units of m^2/s of call
+#' @param D_c diffusion coefficient for CO2 in air in units of m^2/s of class
 #' \code{units}
-#' @param D_w diffusion coefficient for H2O in air in units of m^2/s of call
+#' @param D_w diffusion coefficient for H2O in air in units of m^2/s of class
 #' \code{units}
-#' @param a exponent used for conversion. Use 1 for still air; 0.67 for laminar flow (Jones 2014).
+#' @param a exponent used for conversion. Use 1 for still air; 0.67 for laminar flow (Jones 2014). Should be unitless.
+#' @param unitless Logical. Should scientific units of arguments be checked and set? TRUE is safer, but slower. If FALSE, values provided are assumed to be in correct units.
 #'
 #' \code{units}
 #'
@@ -26,13 +27,15 @@
 #' Jones H. 2014. Plants and Microclimate (3rd edition). Cambridge University Press.
 #'
 #' @examples
+#' library(units)
 #' D_c = set_units(1.29e-05, "m^2/s")
 #' D_w = set_units(2.12e-05, "m^2/s")
 #' g_c = set_units(3, "umol/m^2/s/Pa")
-#' g_w = gc2gw(g_c, D_c, D_w, unitless = FALSE)
+#' a = 1
+#' g_w = gc2gw(g_c, D_c, D_w, a, unitless = FALSE)
 #' g_w
 #'
-#' gw2gc(g_w, D_c, D_w, unitless = FALSE)
+#' gw2gc(g_w, D_c, D_w, a, unitless = FALSE)
 #' @export
 #'
 
@@ -47,12 +50,15 @@ gw2gc = function(
     if (is(g_w, "units")) g_w %<>% drop_units()
     if (is(D_c, "units")) D_c %<>% drop_units()
     if (is(D_w, "units")) D_w %<>% drop_units()
+    if (is(a, "units")) a %<>% drop_units()
     g_c = g_w * (D_c / D_w) ^ a
     return(g_c)
   } else {
     g_w %<>% set_units("umol/m^2/s/Pa")
     D_c %<>% set_units("m^2/s")
     D_w %<>% set_units("m^2/s")
+    # The exponent should be unitless
+    if (is(a, "units")) a %<>% drop_units()
     g_c = g_w * (D_c / D_w) ^ a
     return(g_c)
   }
@@ -62,7 +68,6 @@ gw2gc = function(
 #'
 #' @rdname gw2gc
 #'
-#' @inheritParams gw2gc
 #' @param g_c conductance to CO2 in units (\eqn{\mu}mol H2O / (m^2 s Pa)) of
 #' class \code{units}.
 #' @export
@@ -75,16 +80,19 @@ gc2gw = function(
     unitless,
     a
 ) {
-    if (unitless) {
+  if (unitless) {
     if (is(g_c, "units")) g_c %<>% drop_units()
     if (is(D_c, "units")) D_c %<>% drop_units()
     if (is(D_w, "units")) D_w %<>% drop_units()
+    if (is(a, "units")) a %<>% drop_units()
     g_w = g_c * (D_w / D_c) ^ a
     return(g_w)
   } else {
     g_c %<>% set_units(umol / m^2 / s / Pa)
     D_c %<>% set_units(m^2 / s)
     D_w %<>% set_units(m^2 / s)
+    # The exponent should be unitless
+    if (is(a, "units")) a %<>% drop_units()
     g_w = g_c * (D_w / D_c) ^ a
     return(g_w)
   }
